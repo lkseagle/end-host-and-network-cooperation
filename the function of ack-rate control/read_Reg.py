88fb9c3f30d03169dbe5a5b65860ef0ea04279cc
lkseagle <58589717+lkseagle@users.noreply.github.com>
2023-12-06 20:00:49 +0800

    Add files via upload

---
+++ the function of ack-rate control/read_Reg.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
+import re
+import subprocess 
+import time
+import sched
+
+p = subprocess.Popen('simple_switch_CLI --thrift-port 9091',shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True) 
+p.stdin.write('register_read all_cur_cwnd_reg') 
+out,err = p.communicate()
+fl = "./s2_qlength.txt"
+if os.path.exists(fl):
+	os.remove(fl) 
+with open(fl,"a") as file:
+	file.write(out)
